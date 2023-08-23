0e0d8695df0ba9d87988e69e5bba5b18f1b6fc6b
chelond <142936713+chelond@users.noreply.github.com>
2023-08-24 00:34:01 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
-
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
 
 btn1.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
@@ -57,7 +58,27 @@ btn4.addEventListener("click", function(){
 	}
 });
 
+btn5.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 5!");
+		item = "5";
+		tg.MainButton.show();
+	}
+});
 
+btn6.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 6!");
+		item = "6";
+		tg.MainButton.show();
+	}
+});
 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
@@ -70,6 +91,6 @@ let usercard = document.getElementById("usercard");
 let p = document.createElement("p");
 
 p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name }`;
+${tg.initDataUnsafe.user.last_name}`;
 
 usercard.appendChild(p);
